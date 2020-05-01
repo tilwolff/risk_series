ac4f0f2b89436a72da5e4b0f9df0c98450ac9716
Tilman Wolff-Siemssen <tws@wolffsiemssen.de>
2020-05-01 22:37:32 +0200

    remove apache_get_headers as that does not work on php-fpm 7.2

--- app/auth.php
+++ app/auth.php
@@ -2,9 +2,8 @@
     
     $token = file_get_contents('./app/.token');
     if($token){
-        $headers = apache_request_headers();
-        $pass=isset($headers['Authorization']);
-        if ($pass) $pass=(trim($token) === trim($headers['Authorization']));
+        $pass=isset($_SERVER['HTTP_AUTHORIZATION']);
+        if ($pass) $pass=(trim($token) === trim($_SERVER['HTTP_AUTHORIZATION']));
         if(!$pass){
 		header("HTTP/1.0 401 Unauthorized");
 		echo '{"success": false, "error_message": "Error: authentication failure."}';
